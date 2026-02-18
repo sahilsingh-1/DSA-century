5a372e16db3c1311dd7212278355985bcdef7afb
sahilsingh-1 <sahil.developer139@gmail.com>
2026-02-18 12:42:34 +0530

    function updated

--- reverse.cpp
+++ reverse.cpp
@@ -1,9 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void reverse(vector<int>& arr, int i)
+{
+
+    if(i>=arr.size()/2) return ;
+    
+    swap(arr[i], arr[arr.size()-1-i]);
+    reverse(arr, i+1);
+
+}
+
 int main() {
 
-    int n =0;
-    cout<<n<<endl;
+    int n ;
+    cout<<"enter the size of array"<<endl;
+    cin>>n ;
+
+    vector<int> arr(n) ;
+    for(int i =0 ; i< n ; i++)
+    {
+        cin>>arr[i];
+    }
+
+    reverse(arr, 0);
+
+    for(int i =0 ; i< n ;i++)
+    {
+        cout<<arr[i]<< " ";
+    }
+
+
 
 }
