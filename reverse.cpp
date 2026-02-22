c8e6d49ed2cf58da21b45453736c260d0f42eb9a
sahilsingh-1 <sahil.developer139@gmail.com>
2026-02-22 23:46:25 +0530

    indentation

--- reverse.cpp
+++ reverse.cpp
@@ -3,12 +3,9 @@ using namespace std;
 
 void reverse(vector<int>& arr, int i)
 {
-
     if(i>=arr.size()/2) return ;
-    
     swap(arr[i], arr[arr.size()-1-i]);
     reverse(arr, i+1);
-
 }
 
 int main() {
@@ -16,20 +13,15 @@ int main() {
     int n ;
     cout<<"enter the size of array"<<endl;
     cin>>n ;
-
     vector<int> arr(n) ;
     for(int i =0 ; i< n ; i++)
     {
         cin>>arr[i];
     }
-
     reverse(arr, 0);
-
     for(int i =0 ; i< n ;i++)
     {
         cout<<arr[i]<< " ";
     }
-
-
-
 }
+
